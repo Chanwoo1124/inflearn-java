41e162584329f221166fbd667fcf157453153682
Chanwoo1124 <lgw22774@gmail.com>
2025-10-12 19:14:39 +0900

    initial commit

---
+++ src/zclass1/Student.java
@@ -0,0 +1,20 @@
+package zclass1;
+
+public class Student {
+    String name;
+    int age;
+    int grade;
+
+    public Student(String name,int age, int grade){
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+
+    }
+
+    public static void main(String[] args) {
+        Student stu1 = new Student("영철",17,80);
+        Student stu2 = new Student("찬우",17,80);
+    }
+}
+
